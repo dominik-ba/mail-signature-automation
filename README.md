00c1f25edf9760b59b869aec0beb07c5cc53cb7a
Dominik <7559494+dominik-ba@users.noreply.github.com>
2025-06-10 18:24:59 +0200

    refactor/enhance with devcontainer

--- README.md
+++ README.md
@@ -4,10 +4,23 @@ xyz
 
 ## usage
 
-do this to use this rep
+locally on your windows machine do this
 
 ```bash
-xyz
+pipenv install
+pipenv shell
+python3 main.py "<name of your signatue file>"
+```
+
+In the devcontainer do this
+```bash
+pipenv run python3 main.py "<name of your signatue file>" -p /workspace/signatures/
+```
+
+e.g.
+```bash
+pipenv run python3 main.py "default (dominik.bartsch@capgemini.com)" -p /workspace/signatures/
+pipenv run python3 main.py "response (dominik.bartsch@capgemini.com)" -p /workspace/signatures/
 ```
 
 ## other things
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from googleapiclient.discovery import build
 # If modifying these SCOPES, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
+
 def get_calendar_service():
     creds = None
     if os.path.exists('token.pickle'):
@@ -22,21 +23,24 @@ def get_calendar_service():
             try:
                 creds.refresh(Request())
             except:
-                flow = InstalledAppFlow.from_client_secrets_file('credentials/credentials.json', SCOPES)
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials/credentials.json', SCOPES)
                 creds = flow.run_local_server(port=0)
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials/credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     return build('calendar', 'v3', credentials=creds)
 
+
 def find_next_urlaub_event(service):
     now = datetime.now(timezone.utc).isoformat() # 'Z' indicates UTC time
     print(f"Now: {now}")
     events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=20, singleEvents=True,
-                                        orderBy='startTime').execute()
+                                          maxResults=20, singleEvents=True,
+                                          orderBy='startTime').execute()
     events = events_result.get('items', [])
 
     for event in events:
@@ -49,12 +53,16 @@ def find_next_urlaub_dates(event):
     start = datetime.strptime(event['start'].get('date'), '%Y-%m-%d')
     end = datetime.strptime(event['end'].get('date'), '%Y-%m-%d')
     return start, end
+    start = datetime.strptime(event['start'].get('date'), '%Y-%m-%d')
+    end = datetime.strptime(event['end'].get('date'), '%Y-%m-%d')
+    return start, end
 
 
 def read_template(signature_name, ext):
-    path = os.path.join(os.path.dirname(__file__), 'templates', f"{signature_name}.{ext}")
+    path = os.path.join(os.path.dirname(__file__),
+                        'templates', f"{signature_name}.{ext}")
 
-    encoding= 'cp1252'
+    encoding = 'cp1252'
     if ext == 'txt':
         encoding = 'utf-16-le'
 
@@ -62,12 +70,13 @@ def read_template(signature_name, ext):
         return f.read()
 
 # function to modify the template - replacing dd1 mm1 yy1 dd2 mm2 yy2 with parameters
+
+
 def modify_template(template, dd1, mm1, yy1, dd2, mm2, yy2):
     return template.replace("dd1", dd1).replace("mm1", mm1).replace("yy1", yy1).replace("dd2", dd2).replace("mm2", mm2).replace("yy2", yy2)
 
-def update_signature(signature_name, ext, content):
-    # Path to the signatures folder
-    signatures_path = os.path.expanduser(r'~\AppData\Roaming\Microsoft\Signatures')
+
+def update_signature(signature_name, ext, content, signatures_path):
 
     # Construct the full path to the signature files
     signatures = {}
@@ -81,37 +90,48 @@ def update_signature(signature_name, ext, content):
     # Write the new content to the HTML file
     print(f"Updating {signatures[ext]}...")
 
-    encoding= 'cp1252'
+    encoding = 'cp1252'
     if ext == 'txt':
         encoding = 'utf-16-le'
     with open(signatures[ext], 'w', encoding=encoding) as f:
         f.write(content)
 
 
-def algorithm(signature_name, start, end):
-    extensions = ['htm', 'rtf', 'txt']
-    for ext in extensions:
-        content = read_template(signature_name, ext)
-        content = modify_template(content, str(start.day), str(start.month), str(start.year), str(end.day), str(end.month), str(end.year))
-        #print(content)
-        update_signature(signature_name, ext, content)
+def algorithm(signature_name, ext, start, end):
+    content = read_template(signature_name, ext)
+    content = modify_template(content, str(start.day), str(start.month), str(
+        start.year), str(end.day), str(end.month), str(end.year))
+    # print(content)
+    return content
 
 
 if __name__ == "__main__":
-    #parsing argparse for signature name
+    # parsing argparse for signature name
     parser = argparse.ArgumentParser()
     # adjust name to grant the possibility to provide more then one name
 
     parser.add_argument("name", help="Name of the signature")
+    # optionally provide a path to the signature file, when not provided, use null
+    parser.add_argument("--path", "-p", default=None,
+                        help='Path to the signature file (default: %USERPROFILE%\\AppData\\Roaming\\Microsoft\\Signatures)')
     args = parser.parse_args()
     name = args.name
+    if not args.path:
+        signatures_path = os.path.expanduser(
+            r'~\AppData\Roaming\Microsoft\Signatures')
+    else:
+        signatures_path = args.path
 
     service = get_calendar_service()
     next_event = find_next_urlaub_event(service)
     if next_event:
         start, end = find_next_urlaub_dates(next_event)
         last_day = end - timedelta(days=1)
-        print(f"Next 'urlaub' event is from {str(start.day)}.{str(start.month)}.{str(start.year)} to {str(last_day.day)}.{str(last_day.month)}.{str(last_day.year)} (incl.)")
-        algorithm(name, start, last_day)
+        print(
+            f"Next 'urlaub' event is from {str(start.day)}.{str(start.month)}.{str(start.year)} to {str(last_day.day)}.{str(last_day.month)}.{str(last_day.year)} (incl.)")
+        extensions = ['htm', 'rtf', 'txt']
+        for ext in extensions:
+            content = algorithm(name, ext, start, last_day)
+            update_signature(name, ext, content, signatures_path)
     else:
         print(f"Found no 'urlaub' event")
